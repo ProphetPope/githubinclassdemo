f38a754e96d5fc12a7dcca665493e2ce5a765069
prophetpope <dpope1que@gmail.com>
2020-01-29 15:05:56 -0500

    added files

---
+++ dp.js
@@ -0,0 +1,25 @@
+var txt1;
+var txt2;
+var txt4;
+
+txt1 ="Darnell";
+txt2 ="  -  01/29/2020";
+txt4 ="Prophetic Anointing"
+
+var txt3 = txt1 + txt2;
+var num1 = 10;
+var select = true;
+
+
+console.log(txt1 + txt2);
+
+console.log("Here is txt3: " + txt3);
+
+console.log(num1 + txt3);
+
+console.log(txt4 + txt1);
+
+
+
+
+
